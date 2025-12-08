1d773b5d78cb3a4e647ee57aded3a1778684e63c
Nam <129306077+petjlos@users.noreply.github.com>
2025-12-08 15:51:33 +0700

    Refactor code and add student information prints

--- Bai3/bai3_11.py
+++ Bai3/bai3_11.py
@@ -1,3 +1,5 @@
+print("Sinh viên : Lê Văn Nam")
+print("Mssv : 245752021610121")
 from math import*
 #x là lãi đơn
 #y là kì 
@@ -5,5 +7,6 @@ from math import*
 x, y, z = map(int, input().split())
 def benefit(n,t,k):
     return k*(1+n*(t/100))
+
 
 print(benefit(x,y,z))
