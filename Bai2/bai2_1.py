da4ee4c72407a1b19f22db4c2c4f3f37f2a3d627
Nam <129306077+petjlos@users.noreply.github.com>
2025-12-08 15:47:25 +0700

    Add student info and clean up bai2_1.py
    Added student information and formatted code.

--- Bai2/bai2_1.py
+++ Bai2/bai2_1.py
@@ -1,3 +1,5 @@
+print("Sinh viên : Lê Văn Nam")
+print("Mssv : 245752021610121")
 from math import*
 
 x, y = map(int, input().split())
@@ -6,6 +8,7 @@ if x > y:
     print('x big')
 else:
     print('y big')
+
 
 
 
