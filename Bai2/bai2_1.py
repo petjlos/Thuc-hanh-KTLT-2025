ad243cd7383e78cc8a693dba2352d2c6bab3628a
Nam <129306077+petjlos@users.noreply.github.com>
2025-12-08 15:59:19 +0700

    Update bai2_1.py

--- Bai2/bai2_1.py
+++ Bai2/bai2_1.py
@@ -1,5 +1,4 @@
-print("Sinh viên : Lê Văn Nam")
-print("Mssv : 245752021610121")
+
 from math import*
 
 x, y = map(int, input().split())
@@ -8,6 +7,7 @@ if x > y:
     print('x big')
 else:
     print('y big')
+
 
 
 
