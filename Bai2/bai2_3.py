03c10cb994cad59a5e988dbac37f441b355bbc57
Nam <129306077+petjlos@users.noreply.github.com>
2025-12-08 15:48:50 +0700

    Update bai2_3.py

--- Bai2/bai2_3.py
+++ Bai2/bai2_3.py
@@ -1,3 +1,5 @@
+print("Sinh viên : Lê Văn Nam")
+print("Mssv : 245752021610121")
 from math import*
 
 n = int(input())
@@ -6,4 +8,5 @@ if n % 2 == 0:
     print('chan')
 
 else:
+
     print ('le')
